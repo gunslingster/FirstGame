0e2297efa40614313a3f73c9ad171119d034b401
Sterling <sterlingabrahams@yahoo.com>
2021-05-12 12:28:40 -0700

    Basic player movement and background updated.

--- Classes.py
+++ Classes.py
@@ -16,22 +16,68 @@ class Player(pg.sprite.Sprite):
         self.vel = vec(0,0)
         self.acc = vec(0,0)
         
+    def update(self):
+        # Gravity
+        self.acc = vec(0,0.5)
+        keys = pg.key.get_pressed()
+        if keys[pg.K_RIGHT]:
+            self.acc.x = 0.5
+        if keys[pg.K_LEFT]:
+            self.acc.x = -0.5
+        
+        # Friction
+        self.acc.x += self.vel.x * player_friction
+        
+        # Equations of motion
+        self.vel += self.acc
+        self.pos += self.vel + 0.5 * self.acc
+        
+        # wrap around the sides of the screen
+        if self.pos.x > width:
+            self.pos.x = 0
+        if self.pos.x < 0:
+            self.pos.x = width
+
+        self.rect.midbottom = self.pos
+        
+class Platform(pg.sprite.Sprite):
+    def __init__(self, x, y, w, h):
+        pg.sprite.Sprite.__init__(self)
+        self.image = pg.Surface((w, h))
+        self.image.fill(green)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        
 class Game():
     def __init__(self):
         pg.init()
         self.screen = pg.display.set_mode((width,height))
         pg.display.set_caption(title)
         self.clock = pg.time.Clock()
+        self.bg = pg.transform.scale(pg.image.load('jungle_bg.png'), (width,height))
         self.running = True
     
     def new(self):
         self.all_sprites = pg.sprite.Group()
+        self.platforms = pg.sprite.Group()
         self.player = Player()
         self.all_sprites.add(self.player)
+        p1 = Platform(0, height - 40, width, 40)
+        self.all_sprites.add(p1)
+        self.platforms.add(p1)
+        p2 = Platform(width / 2 - 50, height * 3 / 4, 100, 20)
+        self.all_sprites.add(p2)
+        self.platforms.add(p2)
         self.run()
         
     def update(self):
         self.all_sprites.update()
+        hits = pg.sprite.spritecollide(self.player, self.platforms, False)
+        if hits:
+            self.player.pos.y = hits[0].rect.top
+            self.player.vel.y = 0
+            self.player.rect.midbottom = self.player.pos
     
     def events(self):
         for event in pg.event.get():
@@ -41,7 +87,7 @@ class Game():
                 self.running = False
     
     def draw(self):
-        self.screen.fill(black)
+        self.screen.blit(self.bg, (0,0))
         self.all_sprites.draw(self.screen)
         pg.display.flip()
     
@@ -51,6 +97,7 @@ class Game():
             self.events()
             self.update()
             self.draw()
+            self.clock.tick(fps)
                 
         
 
